ea9d9ca3ac8fe107113510ca7f34ef0716d321da
Andrew Newton <nootn@hotmail.com>
2024-04-23 01:19:05 +1000

    Theme switching

---
+++ BlazorMulti/BlazorMultiUser/BlazorMultiUser.Web/BlazorMultiUser.Web.Client/ThemeInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorMultiUser.Web.Client
+{
+    public static class ThemeInfo
+    {
+        public static readonly string DefaultTheme = "#2b579a";
+        public static readonly string CounterTheme = "#217346";
+    }
+}
